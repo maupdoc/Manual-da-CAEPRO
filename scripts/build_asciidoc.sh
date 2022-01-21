a5aef0d2f555eebed30c07a98fc823311b83bbc1
Mauricio Ferraz <maupdoc@gmail.com>
2022-01-20 23:14:24 -0300

    Scripts improved

--- scripts/build_asciidoc.sh
+++ scripts/build_asciidoc.sh
@@ -2,6 +2,7 @@
 
 # A partir do root
 asciidoc_build_path="asciidoc_build"
+sed -i -e '/^######/ ! s/^#/##/' ${asciidoc_build_path}/book.md
 sed -i -e '1i# MANUAL DA CAEPRO\n' ${asciidoc_build_path}/book.md 
 kramdoc ${asciidoc_build_path}/book.md
 asciidoctor-pdf ${asciidoc_build_path}/book.adoc
